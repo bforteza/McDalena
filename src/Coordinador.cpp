e62b246e2cadd9b0b1b3d9f16a27b05df242bf35
bforteza <b.forteza@alumnos.upm.es>
2024-06-06 11:10:42 +0200

    nada

--- src/Coordinador.cpp
+++ src/Coordinador.cpp
@@ -41,7 +41,7 @@ Coordinador::Coordinador(GLdouble _ancho, GLdouble _alto) :
 	
 	actual = &Menu;
 
-	Menu.add_boton(Boton(300, 40, 0, 0, "bin/imagenes/start.png", "bin/imagenes/startrojo.png", std::bind(&Coordinador::set_unicopeon, this)));
+	Menu.add_boton(Boton(300, 40, 0, 0, "bin/imagenes/UnpeonNegro.png", "bin/imagenes/UnpeonBlanco.png", std::bind(&Coordinador::set_unicopeon, this)));
 	Menu.add_boton(Boton(200, 40, 0, -70, "bin/imagenes/speed.png", "bin/imagenes/speedrojo.png", std::bind(&Coordinador::set_speedchess, this)));
 	Menu.add_boton(Boton(700, 90, 0, 140, "bin/imagenes/pixelchess.png", "bin/imagenes/pixelchess.png", nullptr));
 
